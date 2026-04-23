2667758f863497861844e402a8955938b3b9c6b4
AXIOM <ai.axiom1.0@gmail.com>
2026-04-23 14:58:43 -0400

    Initial commit: Next.js boilerplate

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>MC Global Insurance</h1>
+      <p>Website under development.</p>
+      <p>Source code is now synced to GitHub: <strong>Axiomai007/mcglobal-website</strong></p>
+    </div>
+  );
+}
