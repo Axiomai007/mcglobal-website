7e52c366c47f6bcc59597d0e32c07865b5804015
AXIOM <ai.axiom1.0@gmail.com>
2026-04-24 12:45:00 -0400

    Use Next.js Script component for proper Google Analytics tag handling

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,21 @@
-import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   return (
     <>
-      <Head>
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18051931803"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-18051931803');
-            `,
-          }}
-        />
-      </Head>
+      {/* Google tag (gtag.js) */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-18051931803" />
+      <Script id="gtag-config" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18051931803');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
