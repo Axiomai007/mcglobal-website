5001b73da3e2d9eae0402abc278d5e8c9b8e3496
Michael Rodriguez <mrod@Michaels-Mac-mini.local>
2026-04-24 17:53:06 -0400

    Add global Google Ads gtag script IDs and config constant

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,25 @@
 import Script from 'next/script';
 import '../styles/globals.css';
 
+const GOOGLE_ADS_ID = 'AW-18051931803';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
+        id="google-ads-gtag-src"
         strategy="afterInteractive"
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=AW-18051931803"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`}
       />
       <Script
+        id="google-ads-gtag-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-18051931803');
+            gtag('config', '${GOOGLE_ADS_ID}');
           `,
         }}
       />
